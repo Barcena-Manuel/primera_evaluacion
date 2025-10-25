c52d42c2e0e91fc12591cd5d0c38c0186d83d0b6
Manuel <manuel.barcena.sto@gmail.com>
2025-10-25 19:06:43 -0300

    first commit

---
+++ src/test/java/com/mobydigital/primera_evluacion/gestorturnos/GestorturnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobydigital.primera_evluacion.gestorturnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorturnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
